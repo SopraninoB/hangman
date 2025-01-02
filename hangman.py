50eaea4263d0a190377801649431e4208f4261eb
Sopranino <aknipfer@outlook.com>
2025-01-02 11:57:05 -0500

    made hangman :)

--- hangman.py
+++ hangman.py
@@ -1,5 +1,40 @@
 from random import choice
 
-keywords = open("magic.txt").read().splitlines()
+def get_guess():
+    guess = input("What's the letter\n\n").lower()
+    if len(guess) == 1 and guess.isalpha():
+        return guess
+    print("no!")
+    display_status()
+    return get_guess()
+
+def display_status():
+    print(f"Guess {len(wrong) + 1}/6")
+    print()
+    print(''.join(progress))
+    stop_the_count = ', '.join(sorted(wrong)) if len(wrong) else "none"
+    print(f"wrong guesses used: {stop_the_count}\n")
+
+def is_correct():
+    return ''.join(progress).replace(" ", "") == solution
+
+def update_progress(guess):
+    if guess not in solution:
+        wrong.add(guess)
+    for (i, letter) in enumerate(solution):
+        if letter == guess:
+            progress[i] = letter + " "
+
+keywords = open(r"C:\Users\Wassa\Desktop\python\hangman\magic.txt").read().splitlines()
 solution = choice(keywords)
+progress = ["_ "] * len(solution)
+wrong = set()
+
 print(solution)
+while not is_correct() and len(wrong) < 6:
+    display_status()
+    update_progress(get_guess())
+    
+    
+print(''.join(progress))
+print("Yay!" if len(wrong) < 6 else "Boo!")
